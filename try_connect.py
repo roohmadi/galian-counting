554442de09f2789025a5b36d157571aa790eb188
roohmadi <roohmadi@gmail.com>
2024-01-25 15:26:39 +0700

    Create try_connect.py

---
+++ try_connect.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 25 15:02:13 2024
+
+@author: MAT-Admin
+"""
+
+#!/usr/local/bin/python3
+
+import cv2
+import datetime
+import time
+
+
+def reset_attempts():
+    return 50
+
+
+def process_video(attempts):
+
+    while(True):
+        (grabbed, frame) = camera.read()
+
+        if not grabbed:
+            print("disconnected!")
+            camera.release()
+
+            if attempts > 0:
+                time.sleep(5)
+                return True
+            else:
+                return False
+
+
+recall = True
+attempts = reset_attempts()
+
+while(recall):
+    #camera = cv2.VideoCapture("rtsp://<ip><port>/live0.264")
+    camera = cv2.VideoCapture(0)
+
+    if camera.isOpened():
+        print("[INFO] Camera connected at " +
+              datetime.datetime.now().strftime("%m-%d-%Y %I:%M:%S%p"))
+        attempts = reset_attempts()
+        recall = process_video(attempts)
+    else:
+        print("Camera not opened " +
+              datetime.datetime.now().strftime("%m-%d-%Y %I:%M:%S%p"))
+        camera.release()
+        attempts -= 1
+        print("attempts: " + str(attempts))
+
+        # give the camera some time to recover
+        time.sleep(5)
+        continue
