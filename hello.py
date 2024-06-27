226404dcfa3ca99cd3efae8395450374541b2b05
roohmadi <roohmadi@gmail.com>
2024-06-27 16:37:56 +0700

    run another file

---
+++ hello.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 16:21:21 2024
+
+@author: MAT-Admin
+"""
+
+print("Hello from another file python")
---
+++ tes_file.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 15:52:14 2024
+
+@author: MAT-Admin
+"""
+import os
+from configparser import ConfigParser
+config = ConfigParser()
+isExistINI = os.path.exists('frame.ini')
+if isExistINI:
+    print("exist")
+    f = open("frame.ini", "r")
+    status = f.read()
+    print("status: " + status)
+    f.close()
+    if status:
+        try:
+            os.system(f' python hello.py')
+        except FileNotFoundError:
+            print(f"Error: The file 'hello.py' does not exist.")
+else:
+    print("not exist")
+    f = open("frame.ini", "x")
+    f.write("0")
+    f.close()
